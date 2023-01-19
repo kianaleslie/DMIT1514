7bf941b49b737861c929955c21674d30c8a08bf8
kiana <112987031+kianaleslie@users.noreply.github.com>
2023-01-19 14:41:22 -0700

    added lab into repo

---
+++ DMIT1514-Lab1/DMIT1514-Lab1/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DMIT1514_Lab1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D spaceTexture;
+        Vector2 spaceDirection = new Vector2();
+        Rectangle spaceRectangle = new Rectangle();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            spaceTexture = Content.Load<Texture2D>("Space Background");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            _spriteBatch.Begin();
+            // TODO: Add your drawing code here
+            _spriteBatch.Draw(spaceTexture, spaceRectangle = new Rectangle(0, 0, 800, 480), Color.White);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ DMIT1514-Lab1/DMIT1514-Lab1/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new DMIT1514_Lab1.Game1();
+game.Run();
